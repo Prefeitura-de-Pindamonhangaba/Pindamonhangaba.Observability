e91fea61d5f6b669452ac26116de7d3a2d79b246
Abraão <analista.sistemas@pindamonhangaba.sp.gov.br>
2025-02-13 16:10:05 -0300

    fix

--- logs/registra_logs.py
+++ logs/registra_logs.py
@@ -8,11 +8,11 @@ class RegistraLogs:
         with open(caminho, 'a', encoding='utf-8') as arquivo:
             arquivo.write(
                 f'\n=================\n \
-                LOG - {registro['tipo']}\n \
-                Horario_Registro: {time.strftime('%Y-%m-%d %H:%M:%S')}\n \
-                Status_Code: {registro['status_code']}\n \
-                Headers: {registro['headers']} \n \
-                Conteudo: {registro['content'].decode('utf-8')} \n \
-                Tempo_Resposta: {registro['response_time']} \n \
-                URL: {registro['url_final']} \n')
+                LOG - {registro["tipo"]}\n \
+                Horario_Registro: {time.strftime("%Y-%m-%d %H:%M:%S")}\n \
+                Status_Code: {registro["status_code"]}\n \
+                Headers: {registro["headers"]} \n \
+                Conteudo: {registro["content"].decode("utf-8")} \n \
+                Tempo_Resposta: {registro["response_time"]} \n \
+                URL: {registro["url_final"]} \n')
         return
